8f9a6011568c3d1d6a6e02248647a30820aeb184
azulch <alexander.zulch@cgius.com>
2017-09-13 10:08:00 -0600

    package and imports

--- src/main/java/io/rscale/training/company/MessageController.java
+++ src/main/java/io/rscale/training/company/MessageController.java
@@ -1,9 +1,8 @@
+package io.rscale.training.company;
 
-import org.springframework.beans.factory.annotation.Value;  
-import org.springframework.boot.SpringApplication;  
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
