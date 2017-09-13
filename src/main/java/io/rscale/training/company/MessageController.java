735056dce1a62dbb675d0bae7049eb76c0e0e048
azulch <alexander.zulch@cgius.com>
2017-09-13 09:57:08 -0600

    more imports

--- src/main/java/io/rscale/training/company/MessageController.java
+++ src/main/java/io/rscale/training/company/MessageController.java
@@ -1,5 +1,10 @@
 
+import org.springframework.beans.factory.annotation.Value;  
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RestController;
+
 @RefreshScope
 @RestController
 class MessageController {
